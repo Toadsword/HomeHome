c2fe0d70af414bb8d7f61fb53c443110bfd39480
Whenti <l.quentin@sfr.fr>
2019-01-26 00:48:01 +0100

    porte/maison

---
+++ assets/ChrisTutorials/AnimatedDoor/Scripts/AnimatorSetBool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSetBool : MonoBehaviour
+{
+    public string boolName;
+    private Animator animator;
+
+    // Use this for initialization
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Sets the boolean to be active or inactive
+    /// </summary>
+    /// <param name="active"></param>
+    public void SetAnimBool(bool active)
+    {
+        animator.SetBool(boolName, active);
+    }
+
+    public void SetBoolActive()
+    {
+        animator.SetBool(boolName, true);
+    }
+
+    public void SetBoolInactive()
+    {
+        animator.SetBool(boolName, false);
+    }
+}
