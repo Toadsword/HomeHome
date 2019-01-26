502268dd47eb3d8d8762b87a0766b013b44a7890
Duncan Bourquard <bourquardduncan@gmail.com>
2019-01-26 11:56:13 +0100

    Small changes on the lanterne

---
+++ Assets/Scripts/LanterneChan.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanterneChan : MonoBehaviour
+{
+    [SerializeField] float maxScale = 2.5f;
+    [SerializeField] float minScale = 0.0f;
+    private float currentScale;
+    
+    [SerializeField] private float timeForOneScale = 120;
+
+    [SerializeField] private float timeRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRemaining = timeForOneScale * maxScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+        currentScale = timeRemaining / timeForOneScale;
+        if (currentScale > maxScale)
+            currentScale = maxScale;
+        if (currentScale < minScale)
+            currentScale = minScale;
+
+        currentScale += Mathf.Sin(Time.time * 1.5f) * 0.2f;
+        transform.localScale = new Vector3(currentScale, currentScale);
+    }
+
+    public void ResetLanterneTimer()
+    {
+        timeRemaining = timeForOneScale * maxScale;
+    }
+}
