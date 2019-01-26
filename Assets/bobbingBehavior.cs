ed6a030452fe98b0e0969d7fa7936a401d3db25c
Duncan Bourquard <bourquardduncan@gmail.com>
2019-01-26 14:13:07 +0100

    Amelioration du bobbing de la lanterne

---
+++ Assets/bobbingBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bobbingBehavior : MonoBehaviour
+{
+    [SerializeField] private float timetoStopBobbling = 3f;
+    private float timeSinceHorizontal = 0;
+
+    private float bobbingScale;
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceHorizontal += Time.deltaTime;
+        // SI je bouge
+        if (!Mathf.Approximately(GameInput.GetAxisRaw(GameInput.AxisType.VERTICAL), 0) ||
+            !Mathf.Approximately(GameInput.GetAxisRaw(GameInput.AxisType.HORIZONTAL), 0))
+        {
+            timeSinceHorizontal -= Time.deltaTime * 4;
+        }
+
+        if (timeSinceHorizontal < 0)
+            timeSinceHorizontal = 0;
+        if (timeSinceHorizontal > timetoStopBobbling)
+            timeSinceHorizontal = timetoStopBobbling;
+
+        bobbingScale = 1 - (timeSinceHorizontal / timetoStopBobbling);
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Sin(Time.time * 5f) * 30 * bobbingScale));
+    }
+}
