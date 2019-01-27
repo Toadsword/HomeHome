f58f00a7117e81b4de0e7f65b4821929615a6110
brandongonzalez <brand_1995@hotmail.com>
2019-01-27 13:56:56 +0100

    Scene Home Presque finie

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+
+    [SerializeField] GameObject menuOptions;
+
+    [SerializeField] Button btnResume;
+    [SerializeField] Button btnNewGame;
+    [SerializeField] Button btnQuit;
+
+    SoundManager soundManager=null;
+
+    // Start is called before the first frame update
+    void Start() {
+        soundManager = SoundManager.Instance;
+
+        btnQuit.onClick.AddListener(() => {
+            soundManager.PlaySound(SoundManager.SoundList.CLIC);
+            Application.Quit();
+
+        });
+
+        btnNewGame.onClick.AddListener(() => {
+            //?
+            soundManager.PlaySound(SoundManager.SoundList.CLIC);
+        });
+
+        btnResume.onClick.AddListener(() => {
+            //switcher menu pause
+            menuOptions.SetActive(!menuOptions.activeSelf);
+            soundManager.PlaySound(SoundManager.SoundList.CLIC);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            //switcher menu pause
+            menuOptions.SetActive(!menuOptions.activeSelf);
+            soundManager.PlaySound(SoundManager.SoundList.CLIC);
+        }
+    }
+}
